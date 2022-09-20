370ed63bf8a5801007a477dd0f65d0c5a1101366
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2022-09-20 13:05:15 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a433025a-7cb5-455a-a92a-f1b1f7ee0b93';
+export const MODAL_CLOSE_SEC = 2.5;
